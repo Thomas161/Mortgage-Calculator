5637d9bc77ac3353671b157366be36c32c8cd6f2
Tom Magin <magintc@mac.com>
2019-01-13 17:11:26 +1100

    A mortgage App Calculator, first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+var term;
+var apr;
+var amt;
+var mPmt;
+
+window.onload = function() {
+  //  document.getElementById("apr").focus();
+  document.getElementById("sbt").onclick = getValues;
+};
+
+//use toFixed(2) to set the precision of the mPayment. Use it on an int.
+function getValues() {
+  term = document.getElementById("trm").value;
+  apr = document.getElementById("apr").value;
+  amt = document.getElementById("amt").value;
+
+  apr /= 1200;
+  term *= 12;
+  mPmt = calculatePayment();
+  document.getElementById("pmt").value = "$" + mPmt.toFixed(2);
+}
+getValues();
+
+// function validateFields() {
+//   //validation of fields
+//   if ((term, apr, amt == "")) {
+//     alert("Fields cannot be empty");
+//     return false;
+//   }
+// }
+// validateFields();
+
+function calculatePayment() {
+  var payment =
+    (amt * (apr * Math.pow(1 + apr, term))) / (Math.pow(1 + apr, term) - 1);
+  return payment;
+}
+calculatePayment();
