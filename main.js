d53c3e2add23265be87e4b4414e8eb7c7761db08
Tom <Thomas.Magin@Cognizant.com>
2019-01-30 12:59:12 +1100

    Added some linting for coding checking conventions

---
+++ main.js
@@ -0,0 +1,31 @@
+var term;
+var apr;
+var amt;
+var mPmt;
+
+window.onload = function() {
+	document.getElementById("sbt").onclick = getValues;
+};
+
+//use toFixed(2) to set the precision of the mPayment. Use it on an int.
+function getValues() {
+	term = document.getElementById("trm").value;
+	apr = document.getElementById("apr").value;
+	amt = document.getElementById("amt").value;
+
+	apr /= 1200;
+	term *= 12;
+	mPmt = calculatePayment();
+	document.getElementById("pmt").value = mPmt.toFixed(2);
+}
+getValues();
+
+
+
+function calculatePayment() {
+	var payment =
+    (amt * (apr * Math.pow(1 + apr, term))) / (Math.pow(1 + apr, term) - 1);
+
+	return payment;
+}
+calculatePayment();
