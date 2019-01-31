fe693533bcde57663272e97357832802b9a42c14
Tom <Thomas.Magin@Cognizant.com>
2019-01-31 17:09:14 +1100

    Added total owed for life of loan

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ var apr;
 var amt;
 var mPmt;
 
-window.onload = function() {
+
+
+window.onload = function () {
+	document.getElementById("apr").focus();
 	document.getElementById("sbt").onclick = getValues;
 };
 
@@ -12,20 +15,18 @@ function getValues() {
 	term = document.getElementById("trm").value;
 	apr = document.getElementById("apr").value;
 	amt = document.getElementById("amt").value;
-
 	apr /= 1200;
 	term *= 12;
 	mPmt = calculatePayment();
-	document.getElementById("pmt").value = mPmt.toFixed(2);
+	document.getElementById("pmt").value = "$" + mPmt.toFixed(2);
+	document.getElementById("owe").value = "$"+(mPmt*term).toFixed(2);
 }
-getValues();
-
-
 
 function calculatePayment() {
-	var payment =
-    (amt * (apr * Math.pow(1 + apr, term))) / (Math.pow(1 + apr, term) - 1);
-
+	var payment = amt * (apr * Math.pow((1 + apr), term)) / (Math.pow((1 + apr), term) - 1);
+	
 	return payment;
 }
-calculatePayment();
+
+
+
