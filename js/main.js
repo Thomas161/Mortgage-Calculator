cea1854f015d7d5d86fc2c1b09e0b0b8af4b34df
Tom Magin <magintc@mac.com>
2020-03-25 15:28:45 +1100

    reconfiguring proj

--- js/main.js
+++ js/main.js
@@ -1,35 +1,38 @@
-var term, apr, amt, mPmt;
+var term, interest, amount, monthPayment;
 
 window.onload = function() {
-  //  document.getElementById("apr").focus();
   document.getElementById("sbt").onclick = getValues;
 };
 
 //use toFixed(2) to set the precision of the mPayment. Use it on an int.
 function getValues() {
-  term = document.getElementById("trm").value;
-  apr = document.getElementById("apr").value;
-  amt = document.getElementById("amt").value;
+  term = document.getElementById("term").value;
+  interest = document.getElementById("int").value;
+  amount = document.getElementById("amount").value;
 
-  apr /= 1200;
   term *= 12;
-  mPmt = calculatePayment();
-  document.getElementById("pmt").value = "$" + mPmt.toFixed(2);
+  interest /= 1200;
+  monthPayment =
+    (amount * (interest * Math.pow(1 + interest, term))) /
+    (Math.pow(1 + interest, term) - 1);
+  return (document.getElementById("pmt").value = monthPayment.toFixed(2));
 }
 getValues();
 
-// function validateFields() {
-//   //validation of fields
-//   if ((term, apr, amt == "")) {
-//     alert("Fields cannot be empty");
-//     return false;
-//   }
-// }
-// validateFields();
-
-function calculatePayment() {
-  var payment =
-    (amt * (apr * Math.pow(1 + apr, term))) / (Math.pow(1 + apr, term) - 1);
-  return payment;
+function validateFields() {
+  var text;
+  var t = document.getElementById("term");
+  var a = document.getElementById("amount");
+  var i = document.getElementById("int");
+  if (t === null && t === undefined && t === "") {
+    text = "needs an input";
+  } else if (a === null && a === undefined && a === "") {
+    text = "needs an input";
+  } else if (i === null && i === undefined && i === "") {
+    text = "needs an input";
+  } else {
+    text = "";
+  }
+  return (document.getElementById("demo").innerHTML = text);
 }
-calculatePayment();
+validateFields();
