95b64aa3a073fc3642a65aead0c3c3b1550123fe
Tom Magin <magintc@mac.com>
2020-03-31 17:13:30 +1100

    almost done

--- js/main.js
+++ js/main.js
@@ -1,120 +1,84 @@
-var term, interest, amount, monthPayment, fr, err;
-
 window.onload = function() {
   document.getElementById("sbt").onclick = getValues;
+  // document.getElementById("res").onclick = resetValues;
 };
-
+var term, interest, amount, monthPayment;
 //use toFixed(2) to set the precision of the mPayment. Use it on an int.
 function getValues() {
-  validateFields();
   term = document.getElementById("term").value;
   interest = document.getElementById("int").value;
   amount = document.getElementById("amount").value;
 
-  term *= 12;
-  interest /= 1200;
+  if (/^(?!.*\d\d)/.test(term) && (term === "" || term === null)) {
+    console.log("No good");
+    return false;
+  }
+  if (/^(?!.*\d\d)/.test(amount) && (amount === "" || amount === null)) {
+    console.log("No good");
+    return false;
+  }
+  if (
+    /^(?!.*\d\d\d\d\d\d)/.test(interest) &&
+    (interest === "" || interest === null)
+  ) {
+    console.log("No good");
+    return false;
+  } else {
+    term *= 12;
+    interest /= 1200;
+    var one = interest * Math.pow(1 + interest, term);
+    var two = Math.pow(1 + interest, term) - 1;
+    var solution = one / two;
+    monthPayment = amount * solution;
 
-  monthPayment =
-    (amount * (interest * Math.pow(1 + interest, term))) /
-    (Math.pow(1 + interest, term) - 1);
-
-  var f = function() {
-    var a = (document.getElementById("demo").innerHTML = monthPayment.toFixed(
-      0
-    ));
-    let barChart = new Chart(myChart, {
-      type: "bar",
-      data: {
-        labels: ["Monthly Payments"],
-        datasets: [
-          {
-            label: "MP ($)",
-            data: [a],
-            backgroundColor: "green",
-            borderColor: "#777",
-            color: "red",
-            width: "70",
-            height: "70"
-          }
-        ]
-      },
-      options: {
-        title: {
-          display: true,
-          text: "Mortgage Month Payments ($)",
-          fontSize: 25
+    var f = function() {
+      var a = (document.getElementById("demo").innerHTML = monthPayment.toFixed(
+        0
+      ));
+      let barChart = new Chart(myChart, {
+        type: "bar",
+        data: {
+          labels: ["Monthly Payments"],
+          datasets: [
+            {
+              label: "MP ($)",
+              data: [a],
+              backgroundColor: "gold",
+              borderColor: "#777",
+              color: "red",
+              width: "70",
+              height: "70"
+            }
+          ]
         },
-        legend: {
-          position: "right"
-        },
-        layout: {
-          padding: {
-            left: 600,
-            right: 0,
-            bottom: 1000,
-            top: 0
+        options: {
+          title: {
+            display: true,
+            text: "Mortgage Month Payments ($)",
+            fontSize: 25
+          },
+          legend: {
+            position: "right"
+          },
+          layout: {
+            padding: {
+              left: 600,
+              right: 0,
+              bottom: 1000,
+              top: 0
+            }
           }
         }
-      }
-    });
-    console.log("BarChart : ", barChart);
-    return barChart;
-  };
-  console.log("Closure Function : ", f());
-  return f;
+      });
+      console.log(`BarChart : ${new Date()}`, barChart);
+      return barChart;
+    };
+    console.log(`Closure Function : ${new Date()}`, f());
+    return f;
+  }
 }
 
-function validateFields() {
-  var t = document.getElementById("term");
-  var a = document.getElementById("amount");
-  var i = document.getElementById("int");
-
-  if (isNaN(t.value)) {
-    alert("no good");
-  }
-  if (isNaN(a.value)) {
-    alert("no good");
-  }
-  if (isNaN(i.value)) {
-    alert("no good");
-  }
-
-  return true;
+function reload() {
+  location.reload();
+  return false;
 }
-validateFields();
-
-//   .addEventListener("change", validateFields);
-// fr.addEventListener("submit", e => {
-//   var messages = [];
-//   if (isNaN(term) || term === "" || term === null) {
-//      messages.push("no good");
-//     alert("no good");
-//   }
-//   if (isNaN(amount) || amount === "" || amount === null) {
-//     messages.push("no good");
-//   }
-//   if (isNaN(interest) || interest === "" || interest === null) {
-//     messages.push("no good");
-//   }
-//   if (messages.length > 0) {
-//     e.preventDefault();
-//     err.innerText = messages.join();
-//   }
-
-//   //document.getElementById("mess").innerHTML = err;
-// });
-// term = document
-//   .getElementById("term")
-//   .addEventListener("change", validateFields);
-// amount = document
-//   .getElementById("amount")
-//   .addEventListener("change", validateFields);
-// interest = document
-//   .getElementById("int")
-//   .addEventListener("change", validateFields);
-// console.log(term);
-// console.log(amount);
-// console.log(interest);
-
-// fr = document.getElementById("fuzz");
-// err = document.getElementById("mess");
